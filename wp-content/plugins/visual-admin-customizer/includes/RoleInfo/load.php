5c2e361d35294e73de36e5f9cb37bf1808333c8b
cp <chris@cpsharp.net>
2019-08-24 13:55:09 -0400

    some more tweaks

--- wp-content/plugins/visual-admin-customizer/includes/RoleInfo/load.php
+++
@@ -1,3 +0,0 @@
-<?php
-require dirname(__FILE__) . '/RiRoleInfo.php';
-$yeRoleInfo = new RiRoleInfo();
--- wp-content/plugins/visual-admin-customizer/uninstall.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!defined('WP_UNINSTALL_PLUGIN')) {
-	exit;
-}
-
-delete_site_option('ws_vac_customizations');
-delete_site_option('ws_vac_settings');

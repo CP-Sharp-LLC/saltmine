1fbabb2b29a44e3e514caf614f3dba7b635b9a1d
cp <chris@cpsharp.net>
2019-11-19 20:23:06 -0500

    fluidize top login bar

--- wp-content/themes/concept-one/library/js/cpsharp.js
+++ wp-content/themes/concept-one/library/js/cpsharp.js
@@ -10,30 +10,29 @@ jQuery(document).ready(function () {
 
 function checkHeader() {
     var scrollTop = jQuery(document).scrollTop();
-    if(scrollTop == 0){
+    if (scrollTop < 26) {
         jQuery('.mainheader').removeClass('scrolled');
-    }
-    else {
+    } else if (scrollTop >= 26) {
         jQuery('.mainheader').addClass('scrolled');
     }
 
-    setTimeout(checkHeader, 25);
+    setTimeout(checkHeader, 5);
 }
 
-function focusPopups(){
-    jQuery('a[href="#marketplace"]').click(function(){
+function focusPopups() {
+    jQuery('a[href="#marketplace"]').click(function () {
         setTimeout("jQuery('#wpcf7-f669-o3 > form > p > span.wpcf7-form-control-wrap.customer-name > input').focus();", 250);
     });
 
-    jQuery('a[href="#request-demo"]').mouseup(function(){
+    jQuery('a[href="#request-demo"]').mouseup(function () {
         setTimeout("jQuery('#wpcf7-f302-o4 > form > p > span.wpcf7-form-control-wrap.customer-name > input').focus();", 250);
     });
 
-    jQuery('#get-started').mouseup(function(){
+    jQuery('#get-started').mouseup(function () {
         setTimeout("jQuery('#wpcf7-f302-o4 > form > p > span.wpcf7-form-control-wrap.customer-name > input').focus();", 250);
     });
 }
 
-function menuLogoLinkToAnchor(){
+function menuLogoLinkToAnchor() {
     jQuery("#logo > a").attr('href', '#home');
 }
